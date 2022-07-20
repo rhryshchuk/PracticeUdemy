ff8d4c55db818349280e5a2fe6822ce0309fe9ce
Roman Hryshchuk <rhryshchik3332@gmail.com>
2022-07-20 18:09:06 +0300

    Modal Window

--- js/script.js
+++ js/script.js
@@ -96,6 +96,38 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
-
     setClock('.timer', deadline);
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modalCloseBtn = document.querySelector('[data-close]'),
+          modal = document.querySelector('.modal');
+    
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';// Щоб не скролилась сторінка
+        });
+    });
+    function closeModal() {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+    }
+     modalCloseBtn.addEventListener('click', closeModal);
+    // Щоб вікно закривалось якшо клацати мимо нього
+    modal.addEventListener('click', (e) => {
+        if (e.target == modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+    
 });
